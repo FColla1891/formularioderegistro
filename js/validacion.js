72107bcc97265a4574531c2d2de07f4ca2ef37dc
Martín Pereira <veridicotp22@gmail.com>
2023-08-09 14:56:48 -0300

    Función checkPassword

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,19 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const contraseña = document.getElementById('password1').value;
+const nuevacontraseña = document.getElementById('password2').value;
+
+function checkPassword(){
+    return compararContraseña();
+}
+
+function compararContraseña(contraseña, nuevacontraseña){
+    if (contraseña === nuevacontraseña){
+        return showAlertSuccess;
+    }else 
+        return showAlertError;
+}
+
+console.log(checkPassword());
