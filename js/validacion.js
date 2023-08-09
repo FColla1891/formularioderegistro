c227926378317c007e3d09d91c38e2fd2e69ca3a
Martín Pereira <veridicotp22@gmail.com>
2023-08-09 18:48:41 -0300

    Actualice compararContraseña

--- js/validacion.js
+++ js/validacion.js
@@ -6,30 +6,65 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-
-const contraseña = document.getElementById('password1').value;
-const nuevacontraseña = document.getElementById('password2').value;
-
 const checkbox = document.getElementById("terminos");
 const regBtn = document.getElementById("regBtn");
 const errorMensaje = document.getElementById("errorMensaje");
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function compararContraseña(){
-    if (contraseña === nuevacontraseña){
-        return showAlertSuccess();
-    } else {
-        return showAlertError();
-    }
+    return contraseña === nuevacontraseña;
 }
-
-document.addEventListener("DOMContentLoaded", function () {  
-    regBtn.addEventListener("click", function () {
-      if (!checkbox.checked) {
-        alert("Debes aceptar los términos y condiciones");
-      }
-      compararContraseña();
-    });
-  });
-
