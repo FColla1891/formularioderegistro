b5ade685949b8ce98d9ed1c36ca8c283bee2caec
Joaquín Gil <joacogilp@gmail.com>
2023-08-10 14:20:44 -0300

    Se agregaron comentarios

--- js/validacion.js
+++ js/validacion.js
@@ -7,17 +7,20 @@ const errorMensajeCheckbox = document.getElementById("errorMensajeCheckbox");
 //Mensaje de error del Checkbox
 const mensajeCheckbox = "Debes aceptar los términos y condiciones.";
 
+//Refencia a donde se mostrará el mensaje de error de Contraseña
 const errorMensajeContraseña = document.getElementById("errorMensajeContraseña");
 
-
+//Función encargada de mostrar mensaje de éxito al registrarse
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
 
+//Función encargada de mostrar mensaje de error al registrarse
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
+//Verifica y compara constraseñas retornando valor booleano
 function compararContraseña() {
     const contraseña = document.getElementById('password1').value;
     const confirmacionContraseña = document.getElementById('password2').value;
@@ -28,13 +31,16 @@ function compararContraseña() {
     return contraseña === confirmacionContraseña;
   }
 
+//Agrega funcionalidad al botón registrarse y verifica los requisitos
 document.addEventListener("DOMContentLoaded", function () {  
     regBtn.addEventListener("click", function () {
       if (!checkbox.checked) {
         errorMensajeCheckbox.innerHTML = mensajeCheckbox;
+        showAlertError();
       }
       if (!compararContraseña()) {
         errorMensajeContraseña.innerHTML = "Las contraseñas no coinciden.";
+        showAlertError();
       }
     });
   });
