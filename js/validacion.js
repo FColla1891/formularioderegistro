479f3aebd1d1a37662b90c9a0f1d49f48e2078d8
Joaquín Gil <joacogilp@gmail.com>
2023-08-10 13:37:26 -0300

    Correción de funciones e implemento de verificación de contraseña

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,15 @@
+//Referencia al elémento Checkbox de términos y condiciones
+const checkbox = document.getElementById("terminos");
+//Referencia al botón de registro
+const regBtn = document.getElementById("regBtn");
+//Referencia a donde se mostrará el mensaje de error de Checkbox
+const errorMensajeCheckbox = document.getElementById("errorMensajeCheckbox");
+//Mensaje de error del Checkbox
+const mensajeCheckbox = "Debes aceptar los términos y condiciones.";
+
+const errorMensajeContraseña = document.getElementById("errorMensajeContraseña");
+
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -6,30 +18,24 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-
-const contraseña = document.getElementById('password1').value;
-const nuevacontraseña = document.getElementById('password2').value;
-
-const checkbox = document.getElementById("terminos");
-const regBtn = document.getElementById("regBtn");
-const errorMensaje = document.getElementById("errorMensaje");
-
-
-
-function compararContraseña(){
-    if (contraseña === nuevacontraseña){
-        return showAlertSuccess();
-    } else {
-        return showAlertError();
+function compararContraseña() {
+    const contraseña = document.getElementById('password1').value;
+    const confirmacionContraseña = document.getElementById('password2').value;
+    
+    if (contraseña === "" || confirmacionContraseña === "") {
+        errorMensajeContraseña.innerHTML = "Recuerda llenar los campos de contraseña."
     }
-}
+    return contraseña === confirmacionContraseña;
+  }
 
 document.addEventListener("DOMContentLoaded", function () {  
     regBtn.addEventListener("click", function () {
       if (!checkbox.checked) {
-        alert("Debes aceptar los términos y condiciones");
+        errorMensajeCheckbox.innerHTML = mensajeCheckbox;
       }
-      compararContraseña();
+      if (!compararContraseña()) {
+        errorMensajeContraseña.innerHTML = "Las contraseñas no coinciden.";
+      }
     });
   });
 
